7062f3eca321a671e47d67abbe68c9f1de8bc22f
Léa <lezervinia@gmail.com>
2023-01-30 12:37:20 +0100

    Ajout des éléments du menu

--- front-page.php
+++ front-page.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
+<?php
+ 	if ( have_posts() ) :
+ 		while ( have_posts() ) : 
+			the_post();?>
+			<?php the_content('');
+			
+		 endwhile;
+ 	endif;
 
+ ?>
 <?php get_footer(); ?>
