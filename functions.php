3d5559c2080af109d204c96e49847af95d0530b1
Léa <lezervinia@gmail.com>
2023-01-31 17:33:15 +0100

    Modification des fonctions

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@
 		ob_start();
 		ob_end_clean();
 		$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
-		$first_img = $matches [1] [0];
+		$first_img = $matches[1] [0];
 		
 		if(empty($first_img)){ //Defines a default image
 		$first_img = "/images/default.jpg";
