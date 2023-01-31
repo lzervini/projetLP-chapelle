538b604ee14dabede490d9f826032a0e9b4b02b6
Léa <lezervinia@gmail.com>
2023-01-31 17:32:54 +0100

    Modification des tempaltes pers pour chaque page

--- front-page.php
+++ front-page.php
@@ -1,25 +1,65 @@
 <?php get_header(); ?>
-<?php
-   $args = array(
-     'post_type' => 'actualites',
-     'posts_per_page' => 3,
-   );
- query_posts( $args );
- 	if ( have_posts() ) :
-		while ( have_posts() ) : the_post();?>
-		<article class="">
-		<img class="img-card" src="<?php echo catch_that_image()?>"></img>
-			<div class="card_text">
-			<?php the_title( '<h2>', '</h2>' );?>
-			<p><?php echo get_the_excerpt(); ?></p>
-			<div class="">
-					<a class="" href="<?php the_permalink(); ?>"> En savoir plus</a>
+	<h1>Actualités</h1>
+	<?php
+	   $args = array(
+		 'post_type' => 'actualites',
+		 'posts_per_page' => 3,
+	   );
+	   ?>
+	   <div class="card_all">
+		   <?php query_posts( $args );
+		 if ( have_posts() ) :
+			while ( have_posts() ) : the_post();?>
+	
+			<article class="card">
+					<img class="card-image" src="<?php echo catch_that_image()?>"></img>
+				<div class="card-text">
+				<?php the_title( '<h4>', '</h4>' );?>
+				<p><?php echo get_the_excerpt(); ?></p>
+					<a class="card-link" href="<?php the_permalink(); ?>"> En savoir plus</a>
 				</div>
+			</article>
+			<div>
+
+		<?php 
+		if( have_rows( 'services')): ?>
+			<div class="services">
+  		<?php while(have_rows('services')): the_row(); 
+			$img_services = get_sub_field('img_services');
+			$text_services = get_sub_field('text_services');?>
+			<div class="cardService">
+  				<img src="<?php echo $img_services['url']; ?>"></img>  
+				<p> <?php echo $text_services; ?></p>
 			</div>
-		</article>
+		<?php
+
+		endwhile;
+		endif;
+?>
+		</div>
+
 
 	<?php	endwhile;
 	endif;
  ?>
+
+</div>
+<?php 
+
+	if( have_rows( 'services' ) ): ?>
+		<div class="services">
+  			<?php while(have_rows('services')): the_row(); 
+$icone    = get_sub_field('img_services');
+$text    = get_sub_field('text_services');?>
+		<div class="cardService">
+  			<img src="<?php echo $icone['url']; ?>"></img>  
+			<h2> <?php echo $title; ?></h2>
+				<p> <?php echo $text; ?></p>
+		</div>
+	<?php
+			endwhile;
+	endif;
+?>
+
 
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,12 +1,62 @@
 <?php get_header(); ?>
-<?php
- 	if ( have_posts() ) :
+
+<!-- Page demarche -->
+	<?php if (is_page('Liste des démarches')){
+		?><h1> <?php the_title(); ?></h1>
+ 	<?php if ( have_posts() ) :
  		while ( have_posts() ) : 
 			the_post();?>
 			<?php the_content('');
 			
 		endwhile;
- 	endif;
+ 	endif; ?> 
+		<div class="borderDermarche">
+		<?php if( have_rows( 'demarche')): ?>
+		<div class="demarches">
+	  <?php while(have_rows('demarche')): the_row(); 
+		$img_demarche = get_sub_field('logo_demarche');
+		$text_demarche = get_sub_field('titre_demarche');
+		$url_demarche = get_sub_field('lien_demarche')?>
+		<a href="<?php echo $url_demarche?>">
+		<div class="demarche-card">
+			  <img class ="img-demarche" src="<?php echo $img_demarche['url']; ?>"></img>  
+			 <p><?php echo $text_demarche; ?></p>
+		</div>
+	</a>
+	<?php
 
- ?>
-<?php get_footer(); ?>
+	endwhile;
+endif;
+} ?>
+</div>
+
+
+<!-- Page Urbanisme -->
+<?php if (is_page('Urbanisme')){
+		?>
+		<h1> <?php the_title(); ?></h1>
+		<?php
+	   $args = array(
+		 'post_type' => 'urbanisme',
+		 'posts_per_page' => 4,
+	   );
+	   ?>
+	   <div class="card_sujets">
+		   <?php query_posts( $args );
+		 if ( have_posts() ) :
+			while ( have_posts() ) : the_post();?>
+			<div class="cardSeule">
+		<div class="">
+		<?php the_title( '<h4>', '</h4>' );?>
+		<p><?php echo get_the_excerpt(); ?></p>
+			<a class="card-link" href="<?php the_permalink(); ?>"> En savoir plus</a>
+		</div>
+		 </div>
+		 <hr class="littleGreen">
+
+<?php
+		endwhile;
+	endif;
+	}?>
+ 
+ <?php get_footer(); ?>
