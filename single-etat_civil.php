da1b739f2674f1874d8c95229d35a36e69e07d8c
Léa <lezervinia@gmail.com>
2023-02-01 20:49:44 +0100

    Ajout des templates singles

---
+++ single-etat_civil.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Décès
+ * Template Post Type: etat_civil
+ */
+get_header();?>
+
+<section >
+	<div class="marginWidth">
+		<div class="breadcrumbs"typeof="BreadcrumbList" vocab="https://schema.org/">
+			<?php if(function_exists('bcn_display'))
+			{
+				bcn_display();
+			}?>
+		</div>
+		<h2> <?php the_title(); ?></h2>
+		<?php
+ 	if ( have_posts() ) :
+ 		while ( have_posts() ) : 
+			the_post();?>
+			<?php the_content('');?>
+		</div>
+	</section>
+
+		 <?php endwhile;
+ 	endif;
+
+get_footer();
