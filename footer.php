545fc9d6e66997eef6e8067c37bf67b4ebb8a5ea
Léa <lezervinia@gmail.com>
2023-02-01 17:28:57 +0100

    Modification - style footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="footer">
+<?php wp_nav_menu( array(
+					'theme_location' => 'footer-menu',
+					'container'      => false,
+					'menu_class'     => 'footer-list',
+					'depth'          => 1
+				) ); ?>
+    <div class="copy">
+        <p>© 2020 Copyright Mairie de Lachapelle-sous-Chaux - Tous droits réservés </p>
+    </div>
+    </div>
+</footer>
